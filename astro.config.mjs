13694f56afeccdff045e39d8506eeb51875070c4
Dardaul Hoque <rudro1152@gmail.com>
2025-09-13 13:44:46 +0600

    remove vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
+  output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
